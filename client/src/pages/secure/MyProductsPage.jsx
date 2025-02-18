068d3215f4b8f1d6b3aa794ccb1831b815a7c09c
sebe <sebe2k04@gmail.com>
2025-02-18 13:25:07 +0530

    loader --added

--- client/src/pages/secure/MyProductsPage.jsx
+++ client/src/pages/secure/MyProductsPage.jsx
@@ -4,20 +4,25 @@ import { useEffect } from "react";
 import { axiosInstance } from "../../utils/axiosInstance";
 import { useState } from "react";
 import ProductCard from "../../components/ProductCard";
+import Loader from "../../components/Loader"
 
 const MyProductsPage = () => {
     const {user} = useAuth();
     console.log(user)
     const [products,setProducts] = useState([])
+    const [loading,setLoading] = useState(false)
     console.log(products)
     useEffect(()=>{
       const fetchProducts = async() => {
+        setLoading(true)
         try {
           const res = await axiosInstance.get('/api/product');
           setProducts(res.data);
           console.log(res.data)
+          setLoading(false)
 
         } catch (error) {
+          setLoading(false)
           console.log(error)
           
         }
@@ -32,7 +37,9 @@ const MyProductsPage = () => {
             <h1>My Products</h1>
             <Link to={"/secure/products/create"} className="bg-black text-white px-3 py-1 rounded-xl">Create</Link>
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 py-5 md:gap-10 gap-5">
+        {
+          loading ? <Loader/> : (
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 py-5 md:gap-10 gap-5">
           {
             products.map((product)=>{
               return(
@@ -43,6 +50,8 @@ const MyProductsPage = () => {
             })
           }
         </div>
+          )
+        }
         
 
      </div>
